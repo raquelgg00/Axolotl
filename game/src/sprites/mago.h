da929fd5e0209ffd746b23f2b7200eb58548f2e7
raquelgg00 <raquelgarciaguillem@gmail.com>
2021-10-10 19:51:52 +0200

    wiiii

--- game/src/sprites/mago.h
+++ game/src/sprites/mago.h
@@ -15,5 +15,17 @@ extern const u8 spr_mago_2[8 * 29];
 #define SPR_MAGO_3_W 8
 #define SPR_MAGO_3_H 29
 extern const u8 spr_mago_3[8 * 29];
+#define SPR_MAGO_4_W 8
+#define SPR_MAGO_4_H 29
+extern const u8 spr_mago_4[8 * 29];
+#define SPR_MAGO_5_W 8
+#define SPR_MAGO_5_H 29
+extern const u8 spr_mago_5[8 * 29];
+#define SPR_MAGO_6_W 8
+#define SPR_MAGO_6_H 29
+extern const u8 spr_mago_6[8 * 29];
+#define SPR_MAGO_7_W 8
+#define SPR_MAGO_7_H 29
+extern const u8 spr_mago_7[8 * 29];
 
 #endif
